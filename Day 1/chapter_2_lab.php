b03ff86fe79f70216cf79ab0a4230ea9f9cc3641
Mark Lassoff <mark@learntoprogram.tv>
2017-09-21 11:41:44 -0400

    Day 1 Complete
    Day 1 files complete

---
+++ Day 1/chapter_2_lab.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Doing some math</title>
+</head>
+<body>
+    <p>
+        27 * 3 =<br/>
+        19 / 5 =<br/>
+        22 + 3 + 21=<br/>
+        18 +2 * 2=<br/>
+    </p>
+</body>
+</html>
